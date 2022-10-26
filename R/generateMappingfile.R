db7559bc1d6fa941c2fbc04189ab53bc09449af4
orichters <oliverr@pik-potsdam.de>
2022-10-26 10:01:56 +0200

    incorporate checkIIASASubmission into generateMappingfile

--- R/generateMappingfile.R
+++ R/generateMappingfile.R
@@ -1,17 +1,19 @@
 #' Generate mapping from the template containing all the metadata
 #'
 #' @md
-#' @author Falk Benke
+#' @author Falk Benke, Oliver Richters
 #' @param templates either a character or vector of project templates to use as a starting point for
 #'        creating the mapping, valid projects are "AR6", "SHAPE", "NAVIGATE"
 #' @param outputDirectory path to directory to place generated files (default: output)
 #' @param fileName name of the mapping file to be created, if provided, the file is created in the "output" folder,
 #'        otherwise a data frame is returned (optional)
-#' @param commentFileName name of the comments file to be created, if provided,
+#' @param logFile name of the comments file to be created, if provided,
 #'        the file is created in the "output" folder, otherwise a data frame is
 #'        returned (optional)
-#' @param targetVar column name in template containing the target variable names
-#' @param targetUnit column name in template containing the target units (default: Unit)
+#' @param iiasatemplate filename of xlsx or yaml file provided by IIASA
+#' @param targetVar (vector of) column name in template(s) containing the target variable names
+#'        (default: variable)
+#' @param targetUnit column name in template containing the target units (default: unit)
 #' @param remindVar column name in template containing the REMIND variable names (default: r30m44)
 #' @param remindUnit column name in template containing the REMIND units (default: r30m44_unit)
 #' @param factorCol column name in template containing conversion factors (default: r30m44_factor)
@@ -35,8 +37,9 @@
 #' }
 #' @export
 generateMappingfile <- function(templates = NULL, outputDirectory = "output",
-                                fileName = NULL, commentFileName = NULL,
-                                targetVar = "Variable", targetUnit = "Unit",
+                                fileName = NULL, logFile = NULL,
+                                iiasatemplate = NULL,
+                                targetVar = "variable", targetUnit = "unit",
                                 remindVar = "r30m44", remindUnit = "r30m44_unit",
                                 factorCol = "r30m44_factor", weightCol = NULL, spatialCol = "spatial",
                                 model = "REMIND-MAgPIE"
@@ -46,30 +49,41 @@ generateMappingfile <- function(templates = NULL, outputDirectory = "output",
                                 returnBoolean = FALSE, multiple = TRUE, addAllPattern = FALSE)
     if (length(templates) == 0) stop("No template selected, abort.")
   }
+  for (v in c("targetVar", "targetUnit", "remindVar", "remindUnit", "factorCol", "weightCol", "spatialCol")) {
+    assign(v, tolower(get(v)))
+  }
   if (!is.null(outputDirectory) && !dir.exists(outputDirectory)) {
     dir.create(outputDirectory, recursive = TRUE)
   }
+  message("### Generating mapping file based on templates ", paste(templates, collapse = ", "))
+  if (length(targetVar) == 1 && length(templates) > 1) targetVar <- rep.int(targetVar, length(templates))
+  mapping <- NULL
+  comments <- NULL
 
-  .generateMapping <- function(template, fileName = NULL,
-                               remindVar, remindUnit,
-                               targetVar, targetUnit,
-                               factorCol, weightCol,
-                               spatialCol) {
-    dt <- as.data.table(getTemplate(template))
+  for (i in seq_along(templates)) {
+    dt <- as.data.table(getTemplate(templates[i]))
+    names(dt) <- tolower(names(dt))
 
     ## remove to dos and empty mappings
     dt[get(remindVar) == "TODO", (remindVar) := ""]
     dt <- dt[get(remindVar) != ""]
+
+    ## store comments
+    com <- dt[get("comment") != ""]
+    if (nrow(com) > 0) {
+      com <- com[, list("Model" = model, "Scenario" = "All", "Region" = "All", "Variable" = get(targetVar[i]),
+                        "Year" = "All", "Comment" = get("comment"))]
+      comments <- rbind(comments, com)
+    }
+
     ## factor defaults to 1
     dt[is.na(get(factorCol)), (factorCol) := 1]
-
-    ## spatial defaults to "reg+glo"
 
     # remove empty spatial column, as it is interpreted as logical column
     if (spatialCol %in% colnames(dt) && typeof(dt[, get(spatialCol)]) == "logical") {
       dt[, (spatialCol) := NULL]
     }
-
+    # spatial defaults to "reg+glo"
     if (spatialCol %in% colnames(dt)) {
       dt[get(spatialCol) == "", (spatialCol) := "reg+glo"]
     } else {
@@ -81,79 +95,31 @@ generateMappingfile <- function(templates = NULL, outputDirectory = "output",
       warning(sprintf("No unit found for variables %s", paste(noUnit[[remindVar]], collapse = ", ")))
     }
 
-    dt <- dt[, c(remindVar, targetVar, "factor", "weight", "spatial") :=
+    ## remove variables not in iiasatemplate and correct units
+    if (! is.null(iiasatemplate)) {
+      dt <- rbind(checkIIASASubmission(dt, iiasatemplate, logFile = logFile, failOnUnitMismatch = FALSE))
+    }
+
+    dt <- dt[, c(remindVar, targetVar[i], "factor", "weight", "spatial") :=
       list(
         sprintf("%s (%s)", get(remindVar), get(remindUnit)),
-        sprintf("%s (%s)", get(targetVar), get(targetUnit)),
+        sprintf("%s (%s)", get(targetVar[i]), get(targetUnit)),
         get(factorCol),
-        ifelse(is.null(weightCol), "NULL", get(weightCol)),
+        ifelse(length(weightCol) == 0, "NULL", get(weightCol)),
         get(spatialCol)
       )][
-      , c(remindVar, targetVar, "factor", "weight", "spatial"),
+      , c(remindVar, targetVar[i], "factor", "weight", "spatial"),
       with = FALSE
     ]
-
-    if (!is.null(fileName)) {
-      ## store mapping
-      fwrite(dt, file = paste0(outputDirectory, "/", fileName), sep = ";")
-    }
-    return(dt)
+    setnames(dt, tolower(targetVar[i]), "Variable")
+    mapping <- rbind(mapping, dt)
   }
 
-  .storeComments <- function(template, remindVar, targetVar, model, fileName = NULL) {
-    dt <- as.data.table(getTemplate(template))
-    dt[get(remindVar) == "TODO", (remindVar) := ""]
-    dt <- dt[get(remindVar) != ""]
-
-    comments <- dt[get("Comment") != ""]
-
-    if (nrow(comments) > 0) {
-      comments <- comments[, list(
-        "Model" = model,
-        "Scenario" = "All",
-        "Region" = "All",
-        "Variable" = get(targetVar),
-        "Year" = "All",
-        "Comment" = get("Comment")
-      )]
-
-      if (!is.null(fileName)) {
-        fwrite(comments, file = paste0(outputDirectory, "/", fileName))
-      }
-      return(comments)
-    }
+  if (!is.null(fileName)) {
+    fwrite(mapping, file = paste0(outputDirectory, "/", fileName), sep = ";")
   }
-  if (length(targetVar) == 1 && length(templates) > 1) targetVar <- rep.int(targetVar, length(templates))
-  mapping <- NULL
-  comments <- NULL
-  for (i in seq_along(templates)) {
-    m <- .generateMapping(
-      template = templates[i],
-      remindVar = remindVar,
-      remindUnit = remindUnit,
-      targetVar = targetVar[i],
-      targetUnit = targetUnit,
-      factorCol = factorCol,
-      weightCol = weightCol,
-      spatialCol = spatialCol
-    )
-    setnames(m, targetVar[i], "Variable")
-    mapping <- rbind(mapping, m)
-
-    c <- .storeComments(
-      template = templates[i],
-      remindVar = remindVar,
-      model = model,
-      targetVar = targetVar[i]
-    )
-    comments <- rbind(comments, c)
-
-    if (!is.null(fileName)) {
-      fwrite(mapping, file = paste0(outputDirectory, "/", fileName), sep = ";")
-    }
-    if (!is.null(comments) && !is.null(commentFileName)) {
-      fwrite(comments, file = paste0(outputDirectory, "/", commentFileName), sep = ";")
-    }
+  if (!is.null(comments) && !is.null(logFile)) {
+    fwrite(comments, file = paste0(outputDirectory, "/", logFile), sep = ";")
   }
-  return(list(mappings = mapping, comments = comments))
+  return(invisible(list(mappings = mapping, comments = comments)))
 }
---
+++ R/variableInfo.R
@@ -0,0 +1,92 @@
+#' Provide information on variable, its mappings and summation groups
+#'
+#' @md
+#' @author Oliver Richters
+#' @param varname string with variable name
+#' @param template template shortcut (AR6, NAVIGATE). NULL means all
+#' @param remindVar column name of variable in templates (default: r30m44)
+#' @param remindUnit column name of unit in templates (default: r30m44_unit)
+#' @importFrom stringr str_split str_pad
+#' @importFrom magclass unitsplit
+#' @return prints human-readable summary to the user
+#' @examples
+#' # Simple use. prints human-readable summary to the reader on Emi|CO2:
+#' variableInfo(
+#'   "Emi|CO2"
+#' )
+#' @export
+variableInfo <- function(varname, template = NULL, remindVar = "r30m44", remindUnit = "r30m44_unit") {
+  templates <- templateNames(template)
+
+  green <- "\033[0;32m"
+  blue  <- "\033[0;34m"
+  nc    <- "\033[0m"   # No Color
+  width <- 60
+
+  .getChilds <- function(v, c) {
+    c[which(grepl(paste0("^", gsub("|", "\\|", v, fixed = TRUE), "\\|[^\\|]*$"), gsub("+|", "", c, fixed = TRUE)))]
+  }
+
+  message("\n##### Search for information on ", green, varname, nc, " in mapping templates")
+  for (t in names(templates)) {
+    templateData <- getTemplate(t)
+    template <- basename(templateNames(t))
+    remindno <- which(varname == templateData[, remindVar])
+    exportno <- which(varname == templateData$Variable)
+    if (length(remindno) + length(exportno) == 0) {
+      message("\n### Nothing found in template: ", blue, template, nc)
+      next
+    } else {
+      message("\n### Results from template: ", blue, template, nc)
+    }
+    if (t %in% names(summationsNames())) {
+      summationGroups <- getSummations(t)
+      # print table column names
+      summationsFile <- gsub(".*piamInterfaces", "piamInterfaces", summationsNames(t))
+      message(str_pad(paste0("# Export variable groups found in ",
+                           basename(summationsFile)), width + 8, "right"),
+            "# Corresponding ", remindVar, " variables")
+    } else {
+      summationGroups <- NULL
+      message("# No corresponding summation groups file found, show variables in mapping template.")
+      message("\n", str_pad(paste0("# Export variable"), width + 8, "right"),
+            "# Corresponding ", remindVar, " variables")
+    }
+    for (no in unique(c(remindno, exportno))) {
+      exportname <- templateData$Variable[no]
+      remindname <- templateData[no, remindVar]
+      allexportchilds <- unique(.getChilds(exportname, templateData$Variable))
+      allremindchilds <- unique(.getChilds(varname, templateData[, remindVar]))
+      if (exportname %in% summationGroups$parent) {
+        exportchilds <- summationGroups$child[summationGroups$parent == exportname]
+        # print summation parents
+        message(". ", str_pad(paste(exportname, "="), width + 3, "right"), "   . ",
+                paste0(unitsplit(templateData$r30m44[unitsplit(templateData$Variable)$variable == exportname])$variable,
+                collapse = " + "), " =")
+        # print summation childs
+        for (ch in exportchilds) {
+          remindchilds <- unitsplit(templateData[, remindVar][unitsplit(templateData$Variable)$variable == ch])$variable
+          message("  + ", str_pad(ch, width + 2, "right"), "    + ", paste0(remindchilds, collapse = " + "))
+          allremindchilds <- setdiff(allremindchilds, remindchilds)
+        }
+        allexportchilds <- setdiff(allexportchilds, exportchilds)
+      } else {
+        message(". ", str_pad(paste0(exportname, " (", templateData$Unit[no], ")"), width + 3, "right"),
+                "   . ", remindname, " (", templateData$r30m44_unit[no], ")")
+      }
+      if (length(allexportchilds) + length(allremindchilds) > 0) {
+        message("\n# Child variables", if (t %in% names(summationsNames())) " not in summation group")
+        for (ch in allexportchilds) {
+          remindchilds <- unitsplit(templateData[, remindVar][unitsplit(templateData$Variable)$variable == ch])$variable
+          message("  . ", str_pad(ch, width + 1, "right"), "     . ", paste0(remindchilds, collapse = " + "))
+          allremindchilds <- setdiff(allremindchilds, remindchilds)
+        }
+        for (ch in allremindchilds) {
+          message("   . ", str_pad("NA", width, "right"), "     . ", ch)
+        }
+      }
+      message("Units: ", str_pad(paste0(unique(templateData$Unit[no]), collapse = ", "), width, "right"),
+              " Units: ", paste0(unique(templateData[, remindUnit][no]), collapse = ", "))
+    }
+  }
+}
