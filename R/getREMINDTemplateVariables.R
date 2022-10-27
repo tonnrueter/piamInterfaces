e3f4d60f57f1868cb0ef98757c2e2b16f8b15cbb
Falk Benke <falk.benke@pik-potsdam.de>
2022-10-27 16:51:30 +0200

    refactor function to get REMIND Template Variables

--- R/getREMINDTemplateVariables.R
+++ R/getREMINDTemplateVariables.R
@@ -2,19 +2,22 @@
 #'
 #' @md
 #' @author Falk Benke
+#' @param project name of the project of requested template
 #' @importFrom dplyr %>%
 #' @export
-getREMINDTemplateVariables <- function() {
-  remindVars <- NULL
-  for (t in templateNames()) {
-    template <- getTemplate(t)
+getREMINDTemplateVariables <- function(project) {
+  template <- getTemplate(project)
 
-    if ("exclude_remind2_validation" %in% names(template)) {
-      template <- template %>%
-        filter(is.na(!!sym("exclude_remind2_validation")), !is.na(!!sym("r30m44"))) %>%
-        mutate(!!sym("variable") := paste0(!!sym("r30m44"), " (", !!sym("r30m44_unit"), ")"))
-      remindVars <- c(remindVars, as.character(template[, "variable"]))
-    }
+  remindVars <- NULL
+
+  if ("exclude_remind2_validation" %in% names(template)) {
+    template <- template %>%
+      filter(is.na(!!sym("exclude_remind2_validation")), !is.na(!!sym("r30m44"))) %>%
+      mutate(!!sym("variable") := paste0(!!sym("r30m44"), " (", !!sym("r30m44_unit"), ")"))
+    remindVars <- c(remindVars, as.character(template[, "variable"]))
+  } else {
+    stop("Template ", project, " does not support remind2 validation. \n
+          Add a column `exclude_remind2_validation`!")
   }
   return(unique(remindVars))
 }
