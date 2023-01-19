2094104bb8a52021fda145cbc9865bef9a59def9
orichters <oliverr@pik-potsdam.de>
2023-01-19 11:02:57 +0100

    don't fail tests on unnecessary columns and let them work for NAVIGATE

--- tests/testthat/test-getTemplate.R
+++ tests/testthat/test-getTemplate.R
@@ -2,8 +2,8 @@ test_that("basic checks on main templates", {
   minimalLength <- list("AR6" = 1900, "NAVIGATE" = 1900)
   for (template in names(templateNames())) {
     expect_silent(templateData <- getTemplate(template))
-    expect_true(all(c("idx", "Variable", "Unit", "piam_variable", "piam_unit", "piam_factor",
-                      "internal_comment", "Comment", "Definition") %in% names(templateData)))
+    expect_true(all(c("Variable", "Unit", "piam_variable", "piam_unit", "piam_factor",
+                      "Comment") %in% names(templateData)))
     expect_true(class(templateData) == "data.frame")
     expect_true(length(templateData$Variable) > max(0, unlist(minimalLength[template])))
   }
