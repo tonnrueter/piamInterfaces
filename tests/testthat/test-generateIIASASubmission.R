332d5ef9a1a95e272fd29320a2daa503a2b5abb8
orichters <oliverr@pik-potsdam.de>
2023-01-19 18:48:23 +0100

    add tutorial in readme, speed up tests by 40sec

--- tests/testthat/test-generateIIASASubmission.R
+++ tests/testthat/test-generateIIASASubmission.R
@@ -1,4 +1,4 @@
-for (template in c(setdiff(names(templateNames()), c("AR6", "NAVIGATE")),
+for (template in c(setdiff(names(templateNames()), "NAVIGATE"),
                    list(c("AR6", "AR6_NGFS")), list(c("NAVIGATE", "SHAPE")))) {
   test_that(paste("test generateIIASASubmission with", paste(template, collapse = ",")), {
     vars <- NULL
@@ -12,23 +12,27 @@ for (template in c(setdiff(names(templateNames()), c("AR6", "NAVIGATE")),
     data <- magclass::add_dimension(data, dim = 3.1, add = "model", nm = "REMIND")
     data <- magclass::add_dimension(data, dim = 3.1, add = "scenario", nm = "default")
     magclass::write.report(data, file = file.path(tempdir(), "test.mif"), ndigit = 0)
-    generateMappingfile(templates = unlist(template), outputDir = tempdir(), fileName = "mapping.csv")
     # expect_warning with regexp = NA implies no warning
-    expect_warning(generateIIASASubmission(tempdir(), model = "MAgPIE",
-                                           mappingFile = file.path(tempdir(), "mapping.csv"),
-                                           outputDirectory = file.path(tempdir(), "output"),
-                                           logFile = file.path(tempdir(), "missing.log"),
-                                           outputFilename = "submission1.mif", generateSingleOutput = TRUE),
+    if (all(template == "AR6")) {
+      generateMappingfile(templates = unlist(template), outputDir = file.path(tempdir(), "output"),
+                          fileName = "mapping_AR6.csv")
+      expect_warning(generateIIASASubmission(tempdir(), model = "MAgPIE",
+                                             mappingFile = file.path(tempdir(), "output", "mapping_AR6.csv"),
+                                             outputDirectory = file.path(tempdir(), "output"),
+                                             logFile = file.path(tempdir(), "missing.log"),
+                                             outputFilename = "submission.mif", generateSingleOutput = TRUE),
                    regexp = NA)
-    expect_warning(generateIIASASubmission(tempdir(), model = "REMIND", mapping = template,
-                                           outputDirectory = file.path(tempdir(), "output"),
-                                           logFile = file.path(tempdir(), "missing.log"),
-                                           outputFilename = "submission2.mif", generateSingleOutput = TRUE),
+    } else {
+      expect_warning(generateIIASASubmission(tempdir(), model = "REMIND", mapping = template,
+                                             outputDirectory = file.path(tempdir(), "output"),
+                                             logFile = file.path(tempdir(), "missing.log"),
+                                             outputFilename = "submission.mif", generateSingleOutput = TRUE),
                    regexp = NA)
-    expectedFiles <- file.path(tempdir(), c("mapping.csv", "missing.log", "test.mif",
+    }
+    expectedFiles <- file.path(tempdir(), c("missing.log", "test.mif",
                  file.path("output", c(paste0("mapping_", paste0(template, collapse = "_"), ".csv"),
-                                       "submission1.mif", "submission1.xlsx", "submission2.mif", "submission2.xlsx"))))
-    expect_true(all(file.exists(expectedFiles)))
+                                       "submission.mif", "submission.xlsx"))))
+    for (f in expectedFiles) expect_true(file.exists(f))
     unlink(expectedFiles)
   })
 }
