630dea3651f1fe739b34cc2b0522bd0909858410
orichters <oliverr@pik-potsdam.de>
2023-03-07 18:29:17 +0100

    speed up plotIntercomparison tests

--- tests/testthat/test-plotIntercomparison.R
+++ tests/testthat/test-plotIntercomparison.R
@@ -1,15 +1,19 @@
 test_that("plotComparison works", {
     lPV <- c("Temperature|Global Mean")
     tmpdir <- tempdir()
-    data <- dplyr::filter(quitte::quitte_example_dataAR6, .data$model != "GCAM")
+    data <- dplyr::filter(quitte::quitte_example_dataAR6,
+                          .data$model != "GCAM",
+                          ! .data$variable %in% c("Population", "Final Energy|Industry|Liquids"))
     capture.output(m <- capture_messages(
       plotIntercomparison(data,
                           outputDirectory = tmpdir,
                           lineplotVariables = lPV)))
     expect_match(m, "Add plot for Final Energy", all = FALSE)
     expect_match(m, "Add plot for Temperature", all = FALSE)
-    expect_true(file.exists(file.path(tmpdir, "compare_models_Delayed_transition.pdf")))
-    expect_true(file.exists(file.path(tmpdir, "compare_models_Current_Policies.pdf")))
-    expect_true(file.exists(file.path(tmpdir, "compare_scenarios_REMIND.pdf")))
-    expect_true(file.exists(file.path(tmpdir, "compare_scenarios_MESSAGEix.pdf")))
+    expectedFiles <- c("compare_models_Delayed_transition.pdf", "compare_models_Current_Policies.pdf",
+                       "compare_scenarios_REMIND.pdf", "compare_scenarios_MESSAGEix.pdf")
+    for (ef in expectedFiles) {
+      expect_true(file.exists(file.path(tmpdir, ef)))
+      expect_true(file.info(file.path(tmpdir, ef))$size > 0)
+    }
 })
