26b01615b085a87e2e95526a1b135682cd608644
orichters <oliverr@pik-potsdam.de>
2023-04-17 17:20:22 +0200

    add optional string of variable name to extractReferenceYear to facilitate debugging

--- R/extractReferenceYear.R
+++ R/extractReferenceYear.R
@@ -3,14 +3,16 @@
 #' @md
 #' @author Oliver Richters
 #' @param unit vector or string of units such as 'Index (2020 = 1)'
+#' @param var optional string of variable name to facilitate debugging
 #' @return vector or string of reference years such as '2020'
 #' @export
 
-extractReferenceYear <- function(unit) {
+extractReferenceYear <- function(unit, var = NULL) {
   if (all(grepl("^Index \\([0-9]* = 1\\)", unit))) {
     referenceYear <- gsub("^Index \\(", "", gsub(" = 1\\)", "", unit))
     return(referenceYear)
   } else {
-    stop("Unit ", paste(unit, collapse = ", "), " contains strings not of the form 'Index (2000 = 1)'.")
+    stop("Unit ", paste(unit, collapse = ", "), if (! is.null(var)) paste0(" for ", var),
+         " contains strings not of the form 'Index (2000 = 1)'.")
   }
 }
