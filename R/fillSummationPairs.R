ff4cdd7c5e2ab2e051287f868bdc7a27cdee362f
Renato-Rodrigues <renato.rodrigues@pik-potsdam.de>
2023-09-29 12:01:04 +0200

    updating version numbers and fixing lintr warnings

--- R/fillSummationPairs.R
+++ R/fillSummationPairs.R
@@ -9,41 +9,51 @@
 #' @importFrom quitte as.quitte
 #'
 #' @export
-#' 
-fillSummationPairs <- function(mifFile, summationsFile){
-  
+#'
+fillSummationPairs <- function(mifFile, summationsFile) {
+
   data <- quitte::as.quitte(mifFile, na.rm = TRUE)
 
   summationGroups <- getSummations(summationsFile) %>%
     mutate(parentVar = gsub(" [1-9]$", "", !!sym("parent")))
 
   # summation groups with two child members, in which the parent and one child have reported values
-  pairSummationsGroups <- summationGroups %>% 
-    filter(parent %in% (summationGroups %>% count(parent) %>% filter(n == 2) %>% pull(parent)),
-           parent %in% unique(data$variable)) %>%
-    mutate(childData = ifelse(child %in% unique(data$variable), 1, 0)) %>% 
-    group_by(parent) %>%
-    filter(sum(childData) == 1)
-  
-  # calculated variables for the missing child element of the pair summations 
-  extraVars <- do.call("rbind",lapply(unique(pairSummationsGroups$parent), function(par){
-    childWithValue <- pairSummationsGroups %>% filter(parent == par, childData == 1) %>% pull(child)
-    childWithoutValue <- pairSummationsGroups %>% filter(parent == par, childData == 0) %>% pull(child)
-    message(paste0("adding variable '", childWithoutValue, "' that can be calculated from ", par , " - ", childWithValue))
+  pairSummationsGroups <- summationGroups %>%
+    filter(!!sym("parent") %in%
+             (summationGroups %>% count(!!sym("parent")) %>% filter(!!sym("n") == 2) %>% pull(!!sym("parent"))),
+           !!sym("parent") %in% unique(data$variable)) %>%
+    mutate(!!sym("childData") := ifelse(!!sym("child") %in% unique(data$variable), 1, 0)) %>%
+    group_by(!!sym("parent")) %>%
+    filter(sum(!!sym("childData")) == 1)
+
+  # calculated variables for the missing child element of the pair summations
+  extraVars <- do.call("rbind", lapply(unique(pairSummationsGroups$parent), function(currParent) {
+    childWithValue <- pairSummationsGroups %>%
+      filter(!!sym("parent") == currParent, !!sym("childData") == 1) %>%
+      pull(!!sym("child"))
+    childWithoutValue <- pairSummationsGroups %>%
+      filter(!!sym("parent") == currParent, !!sym("childData") == 0) %>%
+      pull(!!sym("child"))
     tmp <- data %>%
-      filter(variable %in% c(par,childWithValue)) %>%
-      mutate(value = ifelse(variable == childWithValue, - value, value)) %>%
-      group_by(model,scenario,region,unit,period) %>%
-      summarize(value = sum(value), .groups = 'drop') %>%
-      mutate(variable = childWithoutValue) %>%
-      relocate(model,scenario,region,variable,unit,period,value)
-    return(tmp)
+      filter(!!sym("variable") %in% c(currParent, childWithValue)) %>%
+      mutate(!!sym("value") := ifelse(!!sym("variable") == childWithValue, - !!sym("value"), !!sym("value"))) %>%
+      group_by(!!!syms(c("model", "scenario", "region", "unit", "period"))) %>%
+      summarize(!!sym("value") := sum(!!sym("value")), .groups = "drop") %>%
+      mutate(!!sym("variable") := childWithoutValue) %>%
+      group_by(!!!syms(c("model", "scenario", "region", "unit", "period"))) %>%
+      relocate(!!!syms(c("model", "scenario", "region", "variable", "unit", "period", "value")))
+    if (all(tmp %>% pull(!!sym("value")) == 0)) {
+      return()
+    } else {
+      message(paste0("adding variable '", childWithoutValue, "' that can be calculated from ",
+                     currParent, " - ", childWithValue))
+      return(tmp)
+    }
   })
   )
-  
+
   # merge the missing childs data with the original data
-  data <- rbind(data,extraVars)
-  
+  data <- rbind(data, extraVars)
+
   return(data)
 }
-
