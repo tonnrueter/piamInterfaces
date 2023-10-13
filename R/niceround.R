1b38139bb137b3c952f47345929efa8be8a995a2
orichters <oliverr@pik-potsdam.de>
2023-10-13 15:07:18 +0200

    more flexible niceround, improve how sequences of periods are depicted

--- R/niceround.R
+++ R/niceround.R
@@ -1,9 +1,10 @@
 #' niceround returns human-readable string for big and small numbers
-#' If abs(x) > 1, show everything before the dot plus 2 digits after. For abs(x) < 1, show 3 digits
+#' Show everything before the dot and at least 'digits' significant digits
 #' @param x some number
+#' @param digits number of significant digits that are definitely kept
 #' @export
-niceround <- function(x) {
+niceround <- function(x, digits = 3) {
   x <- as.numeric(x)
-  digits <- pmax(3, 2 + ceiling(log10(abs(x))))
+  digits <- pmax(digits, ceiling(log10(abs(x))))
   return(format(signif(x, digits), scientific = FALSE))
 }
