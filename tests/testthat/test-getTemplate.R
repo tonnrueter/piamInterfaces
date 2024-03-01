4ee30e45e42ccd5c0dd2f38b9459d03793e6a822
orichters <oliverr@pik-potsdam.de>
2024-03-01 11:53:43 +0100

    fix merge conflict in template and make sure they never happen again

--- tests/testthat/test-getTemplate.R
+++ tests/testthat/test-getTemplate.R
@@ -1,9 +1,15 @@
-test_that("basic checks on main templates", {
-  minimalLength <- list("AR6" = 1900, "NAVIGATE" = 1900)
-  for (template in names(templateNames())) {
+minimalLength <- list("AR6" = 1900, "NAVIGATE" = 1900)
+for (template in names(templateNames())) {
+  test_that(paste("basic checks on template", template), {
     expect_silent(templateData <- getTemplate(template))
     expect_true(all(c("variable", "unit", "piam_variable", "piam_unit", "piam_factor") %in% names(templateData)))
     expect_true(class(templateData) == "data.frame")
     expect_true(length(templateData$variable) > max(0, unlist(minimalLength[template])))
-  }
-})
+    expect_true(sum(is.na(templateData$variable)) == 0)
+    conflictsigns <- grep("===|<<<|>>>", templateData[, 1], value = TRUE)
+    if (length(conflictsigns) > 0) {
+      warning("Lines that look like merge conflicts:\n", paste(conflictsigns, collapse = "\n"))
+    }
+    expect_true(length(conflictsigns) == 0)
+  })
+}
