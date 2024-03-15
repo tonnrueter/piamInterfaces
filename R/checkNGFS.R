b4e384b251dd2e828e551eb98d59cd70566e6be6
orichters <oliverr@pik-potsdam.de>
2024-03-15 11:28:27 +0100

    actually add checkNGFS script

---
+++ R/checkNGFS.R
@@ -0,0 +1,43 @@
+#' Check NGFS submission by comparing mif data to a template file (xlsx or yaml) provided by IIASA
+#'
+#' @md
+#' @author Oliver Richters
+#' @param mifdata quitte object or filename of mif file
+#' @param iiasatemplate filename of xlsx or yaml file provided by IIASA
+#' @param logFile filename of file for logging. Set to NULL for stdout, set to FALSE for none.
+#' @param generatePlots boolean whether to plot failing summations
+#' @importFrom dplyr filter
+#' @importFrom quitte as.quitte
+#' @importFrom rlang .data
+#' @return quitte object with adapted mif data
+#' @export
+checkNGFS <- function(mifdata, iiasatemplate, logFile, generatePlots = TRUE) {
+  fileprefix <- gsub("\\.[A-Za-z]+$", "", logFile)
+  periods <- seq(2005, 2100, 5)
+  d <- as.quitte(mifdata) %>%
+    filter(.data$period %in% periods) %>%
+    priceIndicesIIASA(iiasatemplate, scenBase = NULL) %>%
+    checkIIASASubmission(iiasatemplate, logFile = NULL, failOnUnitMismatch = FALSE)
+
+  invisible(checkSummations(d, template = NULL, summationsFile = "AR6", logFile = NULL, logAppend = TRUE,
+                            outputDirectory = ".", generatePlots = generatePlots,
+                            dataDumpFile = paste0(fileprefix, "_checkSummations.csv"),
+                            plotprefix = paste0(fileprefix, "_")))
+  csregi <- d %>%
+    checkSummationsRegional(skipUnits = TRUE, skipBunkers = TRUE) %>%
+    rename(World = "total") %>%
+    droplevels()
+  checkyear <- 2050
+  failregi <- csregi %>%
+    filter(abs(.data$reldiff) > 0.5, abs(.data$diff) > 0.00015, .data$period == checkyear) %>%
+    # filter(! .data$variable %in% bunkervars) %>%
+    select(-"model", -"period")
+  if (nrow(failregi) > 0) {
+    message("For those variables, the sum of regional values does not match the World value in 2050:")
+    failregi %>% niceround() %>% print(n = 1000)
+  } else {
+    message("Regional summation checks are fine.")
+  }
+
+  return(d)
+}
