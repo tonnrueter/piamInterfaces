1f1e7c062060702c468835b2d1c048ac49acc08b
orichters <oliverr@pik-potsdam.de>
2024-04-04 17:41:35 +0200

    adjust Elec to Electricity

--- R/renameOldVariables.R
+++ R/renameOldVariables.R
@@ -21,16 +21,15 @@ renameOldVariables <- function(mifdata, variables, logFile = NULL) {
     filter(.data$piam_variable %in% toadd, .data$old_name %in% levels(mifdata$variable))
   old2new <- csvdata$piam_variable
   names(old2new) <- csvdata$old_name
-  # we need to loop because the second calculation might need data from the first
   for (v in names(old2new)) {
     mifdata <- mifdata %>%
       mutate(variable = factor(ifelse(.data$variable == v, old2new[[v]], as.character(.data$variable))))
   }
 
-  if (length(old2new) > 0 && ! is.null(logFile) && ! isFALSE(logFile)) {
-    logtext <- c("Automatically added variables based on renamed_piam_variables.csv:\n",
-                 paste(names(old2new), " -> ", old2new, "\n", collapse = ", "))
-    write(logtext, file = logFile, append = TRUE)
+  if (length(old2new) > 0 && ! isFALSE(logFile)) {
+    logtext <- c("Automatically adjusted variables based on renamed_piam_variables.csv:\n",
+                 paste("-", names(old2new), "->", old2new, collapse = "\n"))
+    if (is.null(logFile)) message(logtext) else  write(logtext, file = logFile, append = TRUE)
   }
 
   return(mifdata)
--- tests/testthat/test-renameOldVariables.R
+++ tests/testthat/test-renameOldVariables.R
@@ -1,7 +1,8 @@
 test_that("renameOldVariables() works", {
   oldvar <- qeAR6
   levels(oldvar$variable)[[1]] <- "Energy Investments|Elec|Wind"
-  newvar <- renameOldVariables(oldvar, variables = "Energy Investments|Electricity|Wind")
+  expect_message(newvar <- renameOldVariables(oldvar, variables = "Energy Investments|Electricity|Wind"),
+                 "Automatically adjusted variables based")
   expect_identical(levels(newvar$variable)[[1]], "Energy Investments|Electricity|Wind")
   expect_identical(nrow(oldvar), nrow(newvar))
   expect_identical(droplevels(select(quitteSort(oldvar), -"variable")),
