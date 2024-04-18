5fbd9baf7eb719d433d46a27dc1a67c343f1ea8a
orichters <oliverr@pik-potsdam.de>
2024-04-18 15:00:34 +0200

    add some NAVIGATE summations

--- tests/testthat/test-getSummations.R
+++ tests/testthat/test-getSummations.R
@@ -1,6 +1,11 @@
 test_that("basic checks on summation Groups", {
   for (template in names(summationsNames())) {
     expect_silent(summationsData <- getSummations(template))
+    duplicates <- filter(summationsData, duplicated(summationsData))
+    if (nrow(duplicates) > 0) {
+      warning("Duplicated line in ", template, ":\n", paste0(duplicates$parent, ";", duplicates$child, collapse = "\n"))
+    }
+    expect_equal(nrow(duplicates), 0)
     expect_true(all(c("parent", "child", "factor") %in% names(summationsData)))
     expect_true(class(summationsData) == "data.frame")
   }
