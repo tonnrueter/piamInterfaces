0a33203a6d93540624f3abbfa896a178080a177a
orichters <oliverr@pik-potsdam.de>
2024-07-10 14:53:07 +0200

    fix getSummations test to make sure it works with different factors

--- tests/testthat/test-getSummations.R
+++ tests/testthat/test-getSummations.R
@@ -1,7 +1,8 @@
 test_that("basic checks on summation Groups", {
   for (template in names(summationsNames())) {
     expect_silent(summationsData <- getSummations(template))
-    duplicates <- select(filter(summationsData, duplicated(summationsData)), c("parent", "child"))
+    duplicates <- select(summationsData, "parent", "child")
+    duplicates <- filter(duplicates, duplicated(duplicates))
     if (nrow(duplicates) > 0) {
       warning("Duplicated line in ", template, ":\n", paste0(duplicates$parent, ";", duplicates$child, collapse = "\n"))
     }
