570038af8c4f86acc404754d410be5a06fbe903a
orichters <oliverr@pik-potsdam.de>
2024-08-08 11:50:08 +0200

    automatically drop aggregated regions

---
+++ tests/testthat/test-dropRegi.R
@@ -0,0 +1,18 @@
+test_that(".dropRegi works", {
+  regiEU21 <- c("DEU", "ECE", "ECS", "ENC", "ESC", "ESW", "EWN", "FRA", "UKI", "NEN", "NES")
+  mifdata <- quitte::as.quitte(data.frame(
+    "model" = "REMIND",
+    "scenario" = "Base",
+    "variable" = "FE",
+    "value" = seq(15),
+    "period" = 2005,
+    "region" = c("USA", "EUR", "NEU", "GLO", regiEU21),
+    "unit" = "EJ/yr"
+  ))
+  expect_identical(mifdata, .dropRegi(mifdata, NULL))
+  expect_identical(mifdata, .dropRegi(mifdata, "nonexistent"))
+  mifdataNoENC <- droplevels(filter(mifdata, ! .data$region %in% "ENC"))
+  expect_identical(mifdataNoENC, .dropRegi(mifdataNoENC, "auto"))
+  removedAggregates <- .dropRegi(mifdata, "auto")
+  expect_setequal(levels(removedAggregates$region), setdiff(levels(mifdata$region), c("EUR", "NEU")))
+})
