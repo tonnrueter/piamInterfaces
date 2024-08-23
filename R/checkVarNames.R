3accadec29414e439ed233c85ea4cad8e212c793
orichters <oliverr@pik-potsdam.de>
2024-08-23 17:20:10 +0200

    adjust areUnitsIdentical

--- R/checkVarNames.R
+++ R/checkVarNames.R
@@ -7,17 +7,17 @@
 #' @export
 checkVarNames <- function(vars) {
 
-  barspace <- grep("[\\| ]{2}|^[\\| ]|[\\| ]$", vars, value = TRUE)
+  barspace <- unique(grep("[\\| ]{2}|^[\\| ]|[\\| ]$", vars, value = TRUE))
   if (length(barspace) > 0) {
     warning("These variable names have wrong bars and spaces: ", paste(barspace, collapse = ", "))
   }
 
-  naVar <- grep("[\\|\\( ]NA[\\|\\) ]|^NA", vars, value = TRUE)
+  naVar <- unique(grep("[\\|\\( ]NA[\\|\\) ]|^NA", vars, value = TRUE))
   if (length(naVar) > 0) {
     warning("These variables and units contain NA: ", paste(naVar, collapse = ", "))
   }
 
-  noUnit <- grep(" \\(.*\\)$", vars, value = TRUE, invert = TRUE)
+  noUnit <- unique(grep(" \\(.*\\)$", vars, value = TRUE, invert = TRUE))
   if (length(noUnit) > 0) {
     warning("These variables have no units: ", paste(noUnit, collapse = ", "))
   }
